915a4427ccc40f79737fede87dc019c070766c35
davzt <azat.davletshin.02@gmail.com>
2022-06-02 21:57:51 +0300

    binary search tree implementation

--- Tree/main.c
+++ Tree/main.c
@@ -1,136 +1,199 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <Windows.h>
+#define COUNT 5
 
-typedef struct tree {
-	int key;
-	struct tree* left;
-	struct tree* right;
-	struct tree* parent;
-} node;
+struct node {
+    int key;
+    struct node *left, *right;
+};
 
-node* create(node* root, int key) {
-	node* tmp = malloc(sizeof(node));
-	tmp->key = key;
-	tmp->parent = NULL;
-	tmp->left = tmp->right = NULL;
-	root = tmp;
-	return root;
-}
+struct node* root = NULL;
 
-node* add(node* root, int key) {
-	node* root2 = root, * root3 = NULL; 
-	node* tmp = malloc(sizeof(node)); 
-	tmp->key = key; 
-	while (root2 != NULL) {
-		root3 = root2;
-		if (key < root2->key) root2 = root2->left;
-		else root2 = root2->right;
-	}
-	tmp->parent = root3;
-	tmp->left = NULL;
-	tmp->right = NULL;
-	if (key < root3->key) root3->left = tmp;
-	else root3->right = tmp;
-	return root;
-}
-
-node* search(node* root, int key) {
-	if ((root == NULL) || (root->key == key)) return root;
-	if (key < root->key) return search(root->left, key);
-	else return search(root->right, key);
-}
-
-node* min(node* root) {
-	node* m = root;
-	while (m->left != NULL) m = m->left;
-	return m;
-}
-
-node* max(node* root) {
-	node* mx = root;
-	while (mx->right != NULL) mx = mx->right;
-	return mx;
-}
-
-node* succ(node* root) {
-	node* p = root, * l = NULL;
-	if (p->right != NULL) return min(p->right);
-	l = p->parent;
-	while ((l != NULL) && (p == l->right)) {
-		p = l;
-		l = l->parent;
-	}
-	return l;
-}
-
-node* delete(node* root, int key) {
-	node* p = root, * l = NULL, * m = NULL;
-	l = search(root, key);
-	if ((l->left == NULL) && (l->right == NULL)) {
-		m = l->parent;
-		if (l == m->right) m->right = NULL;
-		else m->left = NULL;
-		free(l);
-	}
-	if ((l->left == NULL) && (l->right != NULL)) {
-		m = l->parent;
-		if (l == m->right) m->right = l->right;
-		else m->left = l->right;
-		free(l);
-	}
-	if ((l->left != NULL) && (l->right == NULL)) {
-		m = l->parent;
-		if (l == m->right) m->right = l->left;
-		else m->left = l->left;
-		free(l);
-	}
-	if ((l->left != NULL) && (l->right != NULL)) {
-		m = succ(l);
-		l->key = m->key;
-		if (m->right == NULL)
-			m->parent->left = NULL;
-		else m->parent->left = m->right;
-		free(m);
-	}
-	return root;
-}
-
-void preorder(node* root)
+void print(struct node *node, int space)
 {
-	if (root == NULL) return;
-	if (root->key) printf("%d ", root->key);
-	preorder(root->left);
-	preorder(root->right);
+    if (node == NULL)
+        return;
+
+    space += COUNT;
+
+    print(node->right, space);
+
+    printf("\n");
+    for (int i = COUNT; i < space; i++)
+        printf(" ");
+    printf("%d\n", node->key);
+
+    print(node->left, space);
 }
 
-void print(node* tree, int level) {
-	if (tree->right != NULL) print(tree->right, ++level);
-	for (int i = 0; i < (4 * level); i++) printf(" ");
-	printf("%d\n", tree->key);
-	if (tree->left != NULL) print(tree->left, ++level);
-	--level;
+void printTree(struct node *node)
+{
+    print(node, 0);
 }
 
-int main() {
-	node* root = NULL;
-	root = create(root, 8);
-	add(root, 3);
-	add(root, 1);
-	add(root, 6);
-	add(root, 4);
-	add(root, 7);
-	add(root, 10);
-	add(root, 14);
-	add(root, 13);
-	puts("------------------");
-	print(root, 4);
-	puts("------------------");
-	delete(root, 6);
-	print(root, 4);	
-	puts("------------------");
-	add(root, 9);
-	puts("------------------");
-	print(root, 4);
-	return 0;
+struct node* newNode(int item)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
 }
+
+struct node* add(struct node* node, int key)
+{
+    if (node == NULL)
+        return newNode(key);
+
+    if (key < node->key)
+        node->left = add(node->left, key);
+    else
+        node->right = add(node->right, key);
+
+    return node;
+}
+
+struct node* minNode(struct node* node)
+{
+    struct node* current = node;
+
+    while (current && current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+struct node* delete(struct node* node, int key)
+{
+    if (node == NULL)
+        return node;
+    if (key < node->key)
+        node->left = delete(node->left, key);
+    else if (key > node->key)
+        node->right = delete(node->right, key);
+    else {
+        if (node->left == NULL) {
+            struct node* temp = node->right;
+            free(node);
+            return temp;
+        }
+        else if (node->right == NULL) {
+            struct node* temp = node->left;
+            free(node);
+            node = NULL;
+            return temp;
+        }
+        struct node* temp = minNode(node->right);
+        node->key = temp->key;
+        node->right = delete(node->right, temp->key);
+    }
+    return node;
+}
+
+void printInorder(struct node* node)
+{
+    if (node == NULL)
+        return;
+
+    printInorder(node->left);
+    printf("%d ", node->key);
+    printInorder(node->right);
+}
+
+void printPostorder(struct node* node)
+{
+    if (node == NULL)
+        return;
+
+    printPostorder(node->left);
+    printPostorder(node->right);
+    printf("%d ", node->key);
+}
+
+void printPreorder(struct node* node)
+{
+    if (node == NULL)
+        return;
+
+    printf("%d ", node->key);
+    printPreorder(node->left);
+    printPreorder(node->right);
+}
+
+void deleteTree(struct node *node){
+    if (node == NULL)
+        return;
+
+    deleteTree(node->left);
+    deleteTree(node->right);
+    free(node);
+    root = NULL;
+}
+
+int main()
+{
+    int i = 0;
+    while (i != 8) {
+        puts("\nEnter the number of command\n\n  1. Add node\n  2. Delete node\n  3. Inorder traversal\n  "
+             "4. Preorder traversal\n  5. Postorder traversal\n  6. Print the tree\n  7. Delete the tree\n  8. Exit");
+        int k;
+        if (scanf("%d", &i) != 1) {
+            puts("You entered an invalid value");
+            return 0;
+        } else {
+            switch (i) {
+                case 1:
+                    puts("Enter the key of node to insert");
+                    if (scanf("%d", &k) != 1) {
+                        puts("You entered an invalid value");
+                        return 0;
+                    }
+                    root = add(root, k);
+                    break;
+
+                case 2:
+                    puts("Enter the key of node to delete");
+                    if (scanf("%d", &k) != 1) {
+                        puts("You entered an invalid value");
+                        return 0;
+                    }
+                    root = delete(root, k);
+                    break;
+
+                case 3:
+                    printf("Inorder traversal:\n");
+                    printInorder(root);
+                    break;
+
+                case 4:
+                    printf("Preorder traversal:\n");
+                    printPreorder(root);
+                    break;
+
+                case 5:
+                    printf("Postorder traversal:\n");
+                    printPostorder(root);
+                    break;
+
+                case 6:
+                    printf("Tree:\n");
+                    printTree(root);
+                    break;
+
+                case 7:
+                    deleteTree(root);
+                    printf("Tree is deleted\n");
+                    break;
+
+                case 8:
+                    break;
+
+                default:
+                    puts("You entered an invalid value");
+            }
+        }
+    }
+
+    return 0;
+}
+
